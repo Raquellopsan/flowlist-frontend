ca8b29ca46533a30444989ec1395dbe2f1152827
Raquel <raquellopezsancheez@gmail.com>
2025-07-26 11:45:19 +0200

    problema registro

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,8 @@
-import React, { useEffect } from "react";
-import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Registro from "../paginas/Registro";
-import "./App.css";
-import Login from "../paginas/Login";
-import Dashboard from "../paginas/Dashboard";
 
 const App = () => {
-  const navigate = useNavigate();
-
-  // Redirigir según la autenticación
-  useEffect(() => {
-    const userLoggedIn = localStorage.getItem("userLoggedIn");
-
-    if (userLoggedIn === "yes") {
-      navigate("/dashboard"); // Si está logueado, redirige al dashboard
-    } else {
-      navigate("/login"); // Si no está logueado, redirige al login
-    }
-  }, [navigate]); // Dependemos solo de navigate para evitar redirección infinita
-
   return (
     <div className="animated-bg h-screen w-full">
       <Routes>
